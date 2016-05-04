642ec109db6aef6927b2eb5f674a39a7ac45d842
zzastrow2015 <zsz8804@gmail.com>
2016-05-04 14:18:18 -0500

    Update buddy.c

--- proj3/buddy.c
+++ proj3/buddy.c
@@ -50,8 +50,11 @@
  **************************************************************************/
 typedef struct {
 	struct list_head list;
-	/* TODO: DECLARE NECESSARY MEMBER VARIABLES */
+
 	int index;
+	int order;
+	void* address;
+
 } page_t;
 
 /**************************************************************************
@@ -91,7 +94,7 @@ page_t g_pages[(1<<MAX_ORDER)/PAGE_SIZE];
 
 
 
- 	int order = MIN_ORDER;
+ 	int order = MIN_ORDER - 1;
  	int found = 0;
 
  	while(found == 0){
@@ -101,19 +104,19 @@ page_t g_pages[(1<<MAX_ORDER)/PAGE_SIZE];
  			found = 1;
  		}
  	}
-
- 	return order;
+ 	return order + 1;
 
  }
 
- void splitBlock(int order, int pagesIndex){
+ void splitBlock(int order, int pagesIndex, page_t *originalPage){
+
 	 int x;
 	 for(x = pagesIndex; x > order; x--){
-		 if(!list_empty(&free_area[x])){
-			 list_del(free_area[x].next);
-		 }else{
-			 list_add(&g_pages[x].list, &free_area[x]);
-		 }
+
+		 page_t *buddyPage = &g_pages[ADDR_TO_PAGE(BUDDY_ADDR(originalPage->address, (x - 1)))];
+		 buddyPage->order = x - 1;
+		 list_add(&buddyPage->list, &free_area[x - 1]);
+
 	 }
 
  }
@@ -130,6 +133,7 @@ void buddy_init()
 	for (i = 0; i < n_pages; i++) {
 
 		g_pages[i].index = i;
+		g_pages[i].address = PAGE_TO_ADDR(i);
 		INIT_LIST_HEAD(&g_pages[i].list);
 
 	}
@@ -163,10 +167,17 @@ void *buddy_alloc(int size)
 	int requiredOrder = find_order(size);
 
 		int i;
-		for(i = requiredOrder + 1; i <= MAX_ORDER; i++){
+		for(i = requiredOrder; i <= MAX_ORDER; i++){
+
 			if(!list_empty(&free_area[i])){
-				splitBlock(requiredOrder, i);
-				return &free_area[i];
+
+				page_t *originalPage = list_entry(free_area[i].next, page_t, list);
+				list_del(&originalPage->list);
+				INIT_LIST_HEAD(&originalPage->list);
+
+				splitBlock(requiredOrder, i, originalPage);
+				originalPage->order = requiredOrder;
+				return originalPage->address;
 			}
 		}
 
@@ -187,7 +198,68 @@ void *buddy_alloc(int size)
  */
 void buddy_free(void *addr)
 {
-	/* TODO: IMPLEMENT THIS FUNCTION */
+
+	page_t* page = &g_pages[ADDR_TO_PAGE(addr)];
+
+
+	int order = page->order;
+
+
+
+	page_t* buddyPage = &g_pages[ADDR_TO_PAGE(BUDDY_ADDR(page->address, order))];
+	struct list_head *pos;
+	int test = 0;
+	list_for_each(pos, &free_area[order]) {
+		if(buddyPage == list_entry(pos, page_t, list)){
+			test = 1;
+		}
+	}
+
+	if(test == 0){
+
+		list_del(&page->list);
+		INIT_LIST_HEAD(&page->list);
+		list_add(&page->list, &free_area[order]);
+
+	}else{
+
+		if(page > buddyPage){
+			page = buddyPage;
+		}
+
+		while(test == 1){
+
+			list_del(&page->list);
+			INIT_LIST_HEAD(&page->list);
+
+			list_del(&buddyPage->list);
+			INIT_LIST_HEAD(&buddyPage->list);
+
+			order++;
+
+			buddyPage = &g_pages[ADDR_TO_PAGE(BUDDY_ADDR(page->address, order))];
+			test = 0;
+			struct list_head *pos;
+			list_for_each(pos, &free_area[order]) {
+				if(buddyPage == list_entry(pos, page_t, list)){
+					test = 1;
+				}
+
+			}
+
+			if(page > buddyPage){
+				page = buddyPage;
+			}
+
+		}
+
+		list_del(&page->list);
+		INIT_LIST_HEAD(&page->list);
+		page->order = order;
+		list_add(&page->list, &free_area[order]);
+	}
+
+
 }
 
 /**
